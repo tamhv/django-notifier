54660f9a0d76f4bb27efc4663a5961b6b51ae95e
Ingo Berben <ingoberben@gmail.com>
2015-09-04 15:59:36 +0200

    allow HTML emailing

--- notifier/backends.py
+++ notifier/backends.py
@@ -22,7 +22,7 @@ class BaseBackend(object):
 
     def __init__(self, notification, *args, **kwargs):
         self.notification = notification
-        self.template = ('/notifier/%s_%s.txt' %
+        self.template = ('/notifier/%s/%s.txt' %
                          (notification.name, self.name))
 
     # Define how to send the notification
@@ -47,10 +47,13 @@ class EmailBackend(BaseBackend):
         super(EmailBackend, self).__init__(notification, *args, **kwargs)
 
         self.template_subject = (
-            'notifier/%s_%s_subject.txt' % (notification.name, self.name)
+            'notifier/%s/%s_subject.txt' % (notification.name, self.name)
         )
-        self.template_message = (
-            'notifier/%s_%s_message.txt' % (notification.name, self.name)
+        self.template_text_message = (
+            'notifier/%s/%s_text_message.txt' % (notification.name, self.name)
+        )
+        self.template_html_message = (
+            'notifier/%s/%s_html_message.txt' % (notification.name, self.name)
         )
 
     def send(self, user, context=None):
@@ -58,11 +61,16 @@ class EmailBackend(BaseBackend):
 
         subject = render_to_string(self.template_subject, self.context)
         subject = ''.join(subject.splitlines())
-        message = render_to_string(self.template_message, self.context)
+        text_message = render_to_string(self.template_text_message,
+                                        self.context)
+        html_message = render_to_string(self.template_html_message,
+                                        self.context)
 
         try:
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
-                      [user.email])
+            send_mail(subject=subject, message=text_message,
+                      from_email=settings.DEFAULT_FROM_EMAIL,
+                      reciepent_list=[user.email, ],
+                      html_message=html_message)
         except SMTPException:
             return False
         else:
